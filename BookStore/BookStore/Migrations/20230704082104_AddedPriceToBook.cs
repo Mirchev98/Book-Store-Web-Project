c6cae48d922a7f43c4bfa286f1bfd17263201742
Mircho Mirchev <Mircho.Mirchev98@gmail.com>
2023-07-04 11:21:51 +0300

    Added Migration for Book price property

---
+++ BookStore/BookStore/Migrations/20230704082104_AddedPriceToBook.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    public partial class AddedPriceToBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("acfceddf-a1b3-4d16-9997-fc98fd525f78"));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Books",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { new Guid("808c5038-2e78-4f7e-a4ec-c82df020babc"), "Fantasy" });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CategoryId",
+                value: new Guid("808c5038-2e78-4f7e-a4ec-c82df020babc"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("808c5038-2e78-4f7e-a4ec-c82df020babc"));
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Books");
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { new Guid("acfceddf-a1b3-4d16-9997-fc98fd525f78"), "Fantasy" });
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CategoryId",
+                value: new Guid("acfceddf-a1b3-4d16-9997-fc98fd525f78"));
+        }
+    }
+}
