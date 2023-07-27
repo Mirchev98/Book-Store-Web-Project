67ffad3bd9b72846f008c1c0c10e0cc9f62a0cc7
Mircho Mirchev <Mircho.Mirchev98@gmail.com>
2023-07-27 08:10:46 +0300

    Finished functionality for author edit and added func for author details halfway

---
+++ BookStore/BookStore.Web.ViewModels/Author/AuthorDetailsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BookStore.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Web.ViewModels.Author
+{
+    public class AuthorDetailsViewModel
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; } = null!;
+
+        public string ShortBiography { get; set; } = null!;
+
+        public string PhotoUrl { get; set; } = null!;
+    }
+}
