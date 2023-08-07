75eef0552db6c5bea15dc33b281137cd7b2a9886
Mircho Mirchev <Mircho.Mirchev98@gmail.com>
2023-08-07 09:55:14 +0300

    Added custom error pages

--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -55,8 +55,8 @@ namespace BookStore
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Home/Error/500");
+                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
                 app.UseHsts();
             }
 
