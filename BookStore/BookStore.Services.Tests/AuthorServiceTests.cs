fe7a97468786097ee7447a20cbf4eb9159698820
Mircho Mirchev <Mircho.Mirchev98@gmail.com>
2023-08-12 09:59:00 +0300

    Added all author service tests and some book tests

--- BookStore/BookStore.Services.Tests/AuthorServiceTests.cs
+++ BookStore/BookStore.Services.Tests/AuthorServiceTests.cs
@@ -1,13 +1,18 @@
-﻿using BookStore.Data;
+﻿using BookStore.Common;
+using BookStore.Data;
+using BookStore.Data.Models;
+using BookStore.Services.Data;
 using BookStore.Services.Data.Interfaces;
+using BookStore.Web.ViewModels.Author;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace BookStore.Services.Tests
 {
@@ -24,12 +29,82 @@ namespace BookStore.Services.Tests
             this.dbOptions = new DbContextOptionsBuilder<BookStoreDbContext>()
                 .UseInMemoryDatabase("BookStoreInMemory" + Guid.NewGuid().ToString())
                 .Options;
-            this.dbContext = new HouseRentingDbContext(this.dbOptions, false);
+            
+            this.dbContext = new BookStoreDbContext(this.dbOptions, false);
 
             this.dbContext.Database.EnsureCreated();
-            SeedDatabase(this.dbContext);
+            
+            //DatabaseSeeder.Seed(this.dbContext);
 
-            this.agentService = new AgentService(this.dbContext);
+            this.authorService = new AuthorService(this.dbContext);
+        }
+
+        [Test]
+        public async Task GetAuthorByName()
+        {
+            string name = "Robert Jordan";
+            int wantedId = 1;
+
+            Author author = await authorService.GetAuthorByNameAsync(name);
+
+            Assert.That(author.Id, Is.EqualTo(wantedId));
+        }
+
+        [Test]
+        public async Task GetAuthorById()
+        {
+            string name = "Robert Jordan";
+            int wantedId = 1;
+
+            Author author = await authorService.GetAuthorByIdAsync(wantedId);
+
+            Assert.That(author.FullName, Is.EqualTo(name));
+        }
+
+        [Test]
+        public async Task ValidateAuthor()
+        {
+            string name = "Robert Jordan";
+
+            bool result = await authorService.ValidateAuthor(name);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public async Task FillModelById()
+        {
+            string wantedName = "Robert Jordan";
+            int authorId = 1;
+
+            AuthorDetailsViewModel model = new AuthorDetailsViewModel();
+
+            await authorService.FillModelById(model, authorId);
+
+            Assert.That(model.FullName, Is.EqualTo(wantedName));
+        }
+
+        [Test]
+        public async Task GetAllAuthors()
+        {
+            int wantedCount = 4;
+
+            IEnumerable<AllAuthorsViewModel> authors = await authorService.GetAllAuthorsAsync();
+
+            Assert.That(authors.Count(), Is.EqualTo(wantedCount));
+        }
+
+        [Test]
+        public async Task FillModelForEdit()
+        {
+            string wantedName = "Robert Jordan";
+            int authorId = 1;
+            
+            AddAuthorFormModel model = new AddAuthorFormModel();
+
+            await authorService.FillModelById(model, authorId);
+
+            Assert.That(model.FullName, Is.EqualTo(wantedName));
         }
     }
 }
---
+++ BookStore/BookStore.Services.Tests/BookServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using BookStore.Data;
+using BookStore.Services.Data.Interfaces;
+using BookStore.Services.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.Web.ViewModels.Book;
+using BookStore.Services.Data.Models;
+using BookStore.Data.Models;
+
+namespace BookStore.Services.Tests
+{
+    public class BookServiceTests
+    {
+        private DbContextOptions<BookStoreDbContext> dbOptions;
+        private BookStoreDbContext dbContext;
+
+        private IBookService bookService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            this.dbOptions = new DbContextOptionsBuilder<BookStoreDbContext>()
+                .UseInMemoryDatabase("BookStoreInMemory" + Guid.NewGuid().ToString())
+                .Options;
+
+            this.dbContext = new BookStoreDbContext(this.dbOptions, false);
+
+            this.dbContext.Database.EnsureCreated();
+
+            //DatabaseSeeder.Seed(this.dbContext);
+
+            this.bookService = new BookService(this.dbContext);
+        }
+
+        [Test]
+        public async Task AllBook()
+        {
+            int count = 5;
+
+            BookAllQueryModel query = new BookAllQueryModel();
+            AllBooksFilteredAndOrdered model = new AllBooksFilteredAndOrdered();
+
+            model = await bookService.AllAsync(query);
+
+            Assert.That(model.Books.Count(), Is.EqualTo(count));
+        }
+
+        [Test]
+        public async Task FindBook()
+        {
+            int bookId = 1;
+            string wantedTitle = "The Eye of the World";
+
+            var book = bookService.FindBook(bookId);
+
+            Assert.That(book.Title, Is.EqualTo(wantedTitle));
+        }
+    }
+}
